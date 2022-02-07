3a8a761228eba0b963f967abb553e58ee7007568
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2022-02-07 10:11:12 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersRouter = require('./routers/usersRouter');
+const loginRouter = require('./routers/loginRouter');
 
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/user', usersRouter);
+
+app.use('/login', loginRouter);
 
 app.use(errorMiddleware);
 
