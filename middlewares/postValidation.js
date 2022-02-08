91212d8f33fbd7982933855593abc8b206b7ffff
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2022-02-08 11:51:42 -0300

    Requisito 9

--- middlewares/postValidation.js
+++ middlewares/postValidation.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
-const { Category } = require('../models');
-const { HTTP_BAD_REQUEST } = require('../utils/statusCodes');
+const { Category, BlogPost } = require('../models');
+const { HTTP_BAD_REQUEST, HTTP_NOT_FOUND } = require('../utils/statusCodes');
 
 const postsSchema = Joi.object({
   title: Joi.string().required(),
@@ -31,7 +31,19 @@ const validateCategory = async (req, res, next) => {
   return next();
 };
 
+const validatePostExists = async (req, res, next) => {
+  const { id } = req.params;
+  const post = await BlogPost.findByPk(id);
+
+  if (!post) {
+    return res.status(HTTP_NOT_FOUND).json({ message: 'Post does not exist' });
+  }
+
+  return next();
+};
+
 module.exports = {
   validatePost,
   validateCategory,
+  validatePostExists,
 };
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const postsController = require('../controllers/postsController');
 const { verifyAuth } = require('../middlewares/authMiddleware');
-const { validatePost, validateCategory } = require('../middlewares/postValidation');
+const { 
+  validatePost, 
+  validateCategory, 
+  validatePostExists, 
+} = require('../middlewares/postValidation');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', verifyAuth, validatePost, validateCategory, postsController.create);
 router.get('/', verifyAuth, postsController.getAll);
-router.get('/:id', verifyAuth, postsController.getById);
+router.get('/:id', validatePostExists, verifyAuth, postsController.getById);
 
 module.exports = router;
